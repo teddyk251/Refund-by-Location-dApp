de781d027cdab612d0d6dd2a66e0c154fa98076c
Teddy <teddylnk1@gmail.com>
2022-07-19 10:30:51 +0300

    metamask popup

--- admin_panel/lib/main.dart
+++ admin_panel/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:admin_panel/services/metamask.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MaterialApp(
       home: HomePage(),
@@ -34,72 +36,96 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey created above.
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Welcome to the Admin Panel'),
-        actions: [
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Connect Wallet'),
-          )
-        ],
-      ),
-      body: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(30, 30, 30, 0),
-          child: Column(
-            children: <Widget>[
-              TextField(
-                controller: namecontroller,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: "Enter Employee Name"),
+    return ChangeNotifierProvider(
+      create: ((context) => MetaMaskProvider()..init()),
+      builder: (context, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Welcome to the Admin Panel'),
+            actions: [
+              Consumer<MetaMaskProvider>(
+                builder: (context, provider, child) {
+                  late final String text;
+
+                  if (provider.isConnected && provider.isInOperatingChain) {
+                    text = 'Connected';
+                  } else if (provider.isConnected &&
+                      !provider.isInOperatingChain) {
+                    text =
+                        'Wrong chain. Please connect to ${MetaMaskProvider.operatingChain}';
+                  } else if (provider.isEnabled) {
+                    return ElevatedButton(
+                        onPressed: () {
+                          context.read<MetaMaskProvider>().connect();
+                        },
+                        child: const Text('Connect Wallet'));
+                  } else {
+                    text = "Please use a Web3 supported browser";
+                  }
+                };
               ),
-              SizedBox(
-                height: 10,
-              ),
-              TextField(
-                controller: latController,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), hintText: "Enter Latitude"),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextField(
-                controller: lonController,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), hintText: "Enter Longitude"),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextField(
-                controller: distController,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: "Enter allowed distance range"),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    name = namecontroller.text;
-                    latitude = latController.text;
-                    longitude = lonController.text;
-                    distance = distController.text;
-                  });
-                },
-                child: Text('Add Employee'),
-              )
-              // Add TextFormFields and ElevatedButton here.
             ],
           ),
-        ),
-      ),
+          body: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(30, 30, 30, 0),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    controller: namecontroller,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Enter Employee Name"),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    controller: latController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Enter Latitude"),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    controller: lonController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Enter Longitude"),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    controller: distController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Enter allowed distance range"),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        name = namecontroller.text;
+                        latitude = latController.text;
+                        longitude = lonController.text;
+                        distance = distController.text;
+                      });
+                    },
+                    child: Text('Add Employee'),
+                  )
+                  // Add TextFormFields and ElevatedButton here.
+                ],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
